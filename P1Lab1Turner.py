11b09c969be656ff7527a1ec1eec7143eef7bd46
turnerm9773-rgb <turnerm9773@student.faytechcc.edu>
2025-11-13 22:16:48 -0500

    First Python Assignment
    Taking input from user and printing output

---
+++ P1Lab1Turner.py
@@ -0,0 +1,9 @@
+# Micah Turner
+# p1la1.py
+# input and print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello, ", first_name, last_name + "!", "Welcome to CTI-110!")
+  
